162910b23fbe599e02ee1c069d41c41537606dd2
Tomi <somitamas1995@gmail.com>
2023-03-02 11:08:25 +0000

    Add favicon and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,13 @@ document.addEventListener("DOMContentLoaded", function(){
         })
     }
 
+    // Gets the game to check the answer if enter pressed
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     runGame("addition");
 });
 
@@ -20,6 +27,9 @@ document.addEventListener("DOMContentLoaded", function(){
  * and the user's answer has been processed
  */
 function runGame(gameType){
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
